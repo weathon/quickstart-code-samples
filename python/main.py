c3a9322a200e9a36e2781856c7aee106644d2268
Wayne <wguo6358@gmail.com>
2022-06-25 23:33:05 -0500

    log

--- python/main.py
+++ python/main.py
@@ -13,18 +13,16 @@ def exec_statement(conn, stmt):
         return
 
 
-def main():
+def log(msg):
 
     # Connect to CockroachDB
     connection = psycopg2.connect(os.environ['DATABASE_URL'])
 
     statements = [
         # CREATE the messages table
-        "CREATE TABLE IF NOT EXISTS messages (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), message STRING)",
+        "CREATE TABLE IF NOT EXISTS logs (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), time int message STRING)",
         # INSERT a row into the messages table
-        "INSERT INTO messages (message) VALUES ('Hello world!')",
-        # SELECT a row from the messages table
-        "SELECT message FROM messages"
+        "INSERT INTO messages (message) VALUES ('%s')" %msg,
     ]
 
     for statement in statements:
@@ -36,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# https://github.dev/cockroachdb/quickstart-code-samples/blob/main/python/main.py
